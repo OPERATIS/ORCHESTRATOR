63d426311ec4aaf8c05dc5bcd6c14bba7d798f11
sasha.getmanenko <sasha.getmanenko@gmail.com>
2023-08-27 11:11:07 +0300

    Connect shopify (fix)

--- config/connects.php
+++ config/connects.php
@@ -4,6 +4,6 @@ return [
     'shopify' => [
         'apiKey' => env('SHOPIFY_API_KEY'),
         'apiSecret' => env('SHOPIFY_API_SECRET'),
-        'appScopes' => env('SHOPIFY_APP_SCOPES')
+        'appScopes' => explode(',', env('SHOPIFY_APP_SCOPES') ?? '')
     ]
 ];
