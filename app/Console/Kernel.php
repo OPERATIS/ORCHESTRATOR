3aa2bb9a0df1068f11b7ec31606eda6993ae6437
sasha.getmanenko <sasha.getmanenko@gmail.com>
2023-09-22 21:59:23 +0300

    Update Kernel

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,15 +15,24 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
+        // For example 13:15
+        // All orders by period 13:10-13:15
         $schedule->command('shopify:get-orders')->everyFiveMinutes();
+        // All stats by full day
         $schedule->command('facebook:get-stats')->everyFiveMinutes();
+        // All stats by full day
         $schedule->command('google:get-stats')->everyFiveMinutes();
 
+        // For example 13:15
+        // Convert stats to 5 minutes 13:05-13:10
         $schedule->command('google:aggregation-stats')->everyFiveMinutes();
+        // Convert stats to 5 minutes 13:05-13:10
         $schedule->command('facebook:aggregation-stats')->everyFiveMinutes();
 
-        // after delay aggregation
-        $schedule->command('save-metrics')->hourlyAt('25');
+        // For example 13:15
+        // Save metrics by period 12:00-13:00
+        // Init search alerts and then init save analyzes
+        $schedule->command('save-metrics')->cron('15 * * * *');
     }
 
     /**
