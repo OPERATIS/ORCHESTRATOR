37b845381e5f6feacd0a51f3afcdb7ed422d0149
sasha.getmanenko <sasha.getmanenko@gmail.com>
2023-09-22 21:42:30 +0300

    Update logic

--- app/Console/Commands/Facebook/AggregationStats.php
+++ app/Console/Commands/Facebook/AggregationStats.php
@@ -17,21 +17,37 @@ class AggregationStats extends Command
         $startPeriod = Carbon::now()->subMinutes(10)->setSeconds(0)->toDateTimeString();
         $endPeriod = Carbon::now()->subMinutes(5)->setSeconds(0)->toDateTimeString();
 
-        // 10:00:00 10:01:01 10:03:03 10:04:00 10:05:00 10:05:01
-        // 10:01:01 10:03:03 10:04:00 10:05:00
-        $rows = DB::select("
-            SELECT
-                MAX(clicks) - MIN(clicks) as clicks,
-                MAX(impressions) - MIN(impressions) as impressions,
-                MAX(spend) - MIN(spend) as spend,
-                MAX(unique_clicks) - MIN(unique_clicks) as unique_clicks,
-                count(*) as count,
-                connect_id,
-                ad_id
-            FROM fb_stats
-            WHERE start_period >= '{$startPeriod}' and end_period <= '{$endPeriod}'
-            GROUP BY connect_id, ad_id
-        ");
+        if (Carbon::parse($endPeriod)->minute === 0 and Carbon::parse($endPeriod)->hour === 0) {
+            // 23:35 [00:00] 00:05
+            $rows = DB::select("
+                SELECT
+                    MAX(clicks) as clicks,
+                    MAX(impressions) as impressions,
+                    MAX(spend) as spend,
+                    MAX(unique_clicks) as unique_clicks,
+                    2 as count,
+                    connect_id,
+                    ad_id
+                FROM fb_stats
+                WHERE end_period = '{$endPeriod}'
+                GROUP BY connect_id, ad_id
+            ");
+        } else {
+            // 10:00:00 [10:01:01 10:03:03 10:04:00 10:05:00] 10:05:01
+            $rows = DB::select("
+                SELECT
+                    MAX(clicks) - MIN(clicks) as clicks,
+                    MAX(impressions) - MIN(impressions) as impressions,
+                    MAX(spend) - MIN(spend) as spend,
+                    MAX(unique_clicks) - MIN(unique_clicks) as unique_clicks,
+                    count(*) as count,
+                    connect_id,
+                    ad_id
+                FROM fb_stats
+                WHERE start_period >= '{$startPeriod}' and end_period <= '{$endPeriod}'
+                GROUP BY connect_id, ad_id
+            ");
+        }
 
         $aggregationGaStats = [];
         foreach ($rows as $row) {
--- app/Console/Commands/Google/AggregationStats.php
+++ app/Console/Commands/Google/AggregationStats.php
@@ -14,24 +14,42 @@ class AggregationStats extends Command
     public function handle(): bool
     {
         // Every five minutes
-        $startPeriod = Carbon::now()->setSeconds(0)->subMinutes(15)->toDateTimeString();
-        $endPeriod = Carbon::now()->setSeconds(0)->subMinutes(10)->toDateTimeString();
+        $startPeriod = Carbon::now()->subMinutes(10)->setSeconds(0)->toDateTimeString();
+        $endPeriod = Carbon::now()->subMinutes(5)->setSeconds(0)->toDateTimeString();
 
-        $rows = DB::select("
-            SELECT
-                MAX(impressions) - MIN(impressions) as impressions,
-                MAX(pageviews) - MIN(pageviews) as pageviews,
-                MAX(unique_pageviews) - MIN(unique_pageviews) as unique_pageviews,
-                MAX(ad_clicks) - MIN(ad_clicks) as ad_clicks,
-                MAX(ad_cost) - MIN(ad_cost) as ad_cost,
-                count(*) as count,
-                connect_id,
-                unique_table_id,
-                MAX(end_period) as period
-            FROM ga_stats
-            WHERE start_period >= '{$startPeriod}' and end_period <= '{$endPeriod}'
-            GROUP BY connect_id, unique_table_id
-        ");
+        if (Carbon::parse($endPeriod)->minute === 0 and Carbon::parse($endPeriod)->hour === 0) {
+            // 23:35 [00:00] 00:05
+            $rows = DB::select("
+                SELECT
+                    MAX(impressions) as impressions,
+                    MAX(pageviews) as pageviews,
+                    MAX(unique_pageviews) as unique_pageviews,
+                    MAX(ad_clicks) as ad_clicks,
+                    MAX(ad_cost) as ad_cost,
+                    2 as count,
+                    connect_id,
+                    unique_table_id
+                FROM ga_stats
+                WHERE start_period >= '{$startPeriod}' and end_period <= '{$endPeriod}'
+                GROUP BY connect_id, unique_table_id
+            ");
+        } else {
+            // 10:00:00 [10:01:01 10:03:03 10:04:00 10:05:00] 10:05:01
+            $rows = DB::select("
+                SELECT
+                    MAX(impressions) - MIN(impressions) as impressions,
+                    MAX(pageviews) - MIN(pageviews) as pageviews,
+                    MAX(unique_pageviews) - MIN(unique_pageviews) as unique_pageviews,
+                    MAX(ad_clicks) - MIN(ad_clicks) as ad_clicks,
+                    MAX(ad_cost) - MIN(ad_cost) as ad_cost,
+                    count(*) as count,
+                    connect_id,
+                    unique_table_id
+                FROM ga_stats
+                WHERE start_period >= '{$startPeriod}' and end_period <= '{$endPeriod}'
+                GROUP BY connect_id, unique_table_id
+            ");
+        }
 
         $aggregationGaStats = [];
         foreach ($rows as $row) {
@@ -44,7 +62,9 @@ class AggregationStats extends Command
                     'ad_cost' => $row->ad_cost,
                     'connect_id' => $row->connect_id,
                     'unique_table_id' => $row->unique_table_id,
-                    'period' => $row->period
+                    'period' => '5_minutes',
+                    'start_period' => $startPeriod,
+                    'end_period' => $endPeriod,
                 ];
             }
         }
