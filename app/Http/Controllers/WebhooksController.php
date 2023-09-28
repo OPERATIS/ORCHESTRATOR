2dc3741375fd94059efc97462f08176777fa8f19
sasha.getmanenko <sasha.getmanenko@gmail.com>
2023-09-28 22:05:17 +0300

    Update logic messenger

--- app/Http/Controllers/WebhooksController.php
+++ app/Http/Controllers/WebhooksController.php
@@ -54,24 +54,26 @@ class WebhooksController extends Controller
     public function messenger(Request $request)
     {
         if ($request->isJson()) {
-            $response = json_decode($request->json());
-            $message = $response->message->text;
-            $psid = $response->sender->id;
-            $userId = str_replace('Start to notifications #', '', $message);
-            if (is_numeric($userId)) {
-                $meUser = MeUser::updateOrCreate([
-                    'user_id' => $userId,
-                    'psid' => $psid,
-                ]);
+            $response = $request->all();
+            if (isset($response['entry'][0]['messaging'][0]['message']['text'])) {
+                $message = $response['entry'][0]['messaging'][0]['message']['text'];
+                $psid = $response['entry'][0]['messaging'][0]['sender']['id'];
+                $userId = str_replace('Start to notifications #', '', $message);
+                if (is_numeric($userId)) {
+                    $meUser = MeUser::updateOrCreate([
+                        'user_id' => $userId,
+                        'psid' => $psid,
+                    ]);
 
-                if (Carbon::parse($meUser->created_at)->seconds(0)->toDateTimeString() === Carbon::now()->seconds(0)->toDateTimeString()) {
-                    $facebook = new Facebook();
-                    // TODO add text
-                    $facebook->sendMeMessage('Add text', $meUser->psid);
+                    if (Carbon::parse($meUser->created_at)->seconds(0)->toDateTimeString() === Carbon::now()->seconds(0)->toDateTimeString()) {
+                        $facebook = new Facebook();
+                        // TODO add text
+                        $facebook->sendMeMessage('Add text', $meUser->psid);
+                    }
                 }
             }
 
-            return response();
+            return response('');
         } else {
             // Step validation
             $hubChallenge = $request->get('hub_challenge');
