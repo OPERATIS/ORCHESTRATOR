2520bedf85e9f5abc3545be217896d9d18f1e216
sasha.getmanenko <sasha.getmanenko@gmail.com>
2023-10-10 23:38:04 +0300

    Update chat

--- app/Models/Chat.php
+++ app/Models/Chat.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Chat extends Model
@@ -18,5 +19,13 @@ class Chat extends Model
     public function messages(): HasMany
     {
         return $this->hasMany(ChatMessage::class);
+    }
+
+    /**
+     * @return BelongsTo
+     */
+    public function alert(): BelongsTo
+    {
+        return $this->belongsTo(Alert::class);
     }
 }
