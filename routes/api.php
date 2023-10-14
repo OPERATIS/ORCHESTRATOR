92f735b9f4330b15fb23da033a0c60646f403bc0
sasha.getmanenko <sasha.getmanenko@gmail.com>
2023-10-14 20:21:23 +0300

    Remove api

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,5 @@
 <?php
 
-use App\Http\Controllers\Api\AuthController;
-use App\Http\Controllers\Api\MetricsController;
-use App\Http\Controllers\Api\SlackController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,11 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::middleware('auth:sanctum')->group(function () {
-    Route::get('/metrics/actual', [MetricsController::class, 'actualData'])->name('api.metrics.actual');
-    Route::get('/metrics/chart', [MetricsController::class, 'chartData'])->name('api.metrics.chart');
-    Route::get('/slack/login', [SlackController::class, 'login'])->name('api.slack.login');
-    Route::get('/slack/callback', [SlackController::class, 'callback'])->name('api.slack.callback');
-});
 
-Route::post('/auth/register', [AuthController::class, 'registerUser'])->name('api.auth.register');
-Route::post('/auth/login', [AuthController::class, 'loginUser'])->name('api.auth.login');
+});
