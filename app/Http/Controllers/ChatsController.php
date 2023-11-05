b015c492573bf4e0b4971f1e7631aa5daa34bf3f
Vadim Shchur <v.shchur@escharts.com>
2023-11-05 16:22:39 +0200

    fix

--- app/Http/Controllers/ChatsController.php
+++ app/Http/Controllers/ChatsController.php
@@ -139,7 +139,7 @@ class ChatsController extends Controller
                 'status' => true,
                 'chat' => $chat,
                 'messages' => $messages,
-                'showMoreDetails' => count($messages) <= 3
+                'showMoreDetails' => count($messages) < 3
             ]);
         } else {
             return view('chats.index')
